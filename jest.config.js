65fda72a4cdc298d2497a492a255dd155dc3857a
ayumid2023 <141483735+ayumid2023@users.noreply.github.com>
2026-08-16 12:16:42 +0300

    Add Jest configuration for React Native project

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|expo|@expo)/)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/**/*.types.ts',
+    '!src/**/__tests__/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+};
