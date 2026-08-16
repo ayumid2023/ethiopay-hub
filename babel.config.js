906893c48ed99efbec4e6f3829b85c309135aa9f
ayumid2023 <141483735+ayumid2023@users.noreply.github.com>
2026-08-16 12:15:07 +0300

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@types': './src/types',
+            '@hooks': './src/hooks',
+            '@api': './src/api',
+            '@store': './src/store',
+            '@navigation': './src/navigation',
+          },
+        },
+      ],
+      ['module:react-native-dotenv'],
+      ['react-native-reanimated/plugin'],
+    ],
+  };
+};
